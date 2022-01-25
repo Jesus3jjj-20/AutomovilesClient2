b53cc6c62b9bef4e55775d14b44a4cea203fb09b
Jesus3jjj-20 <73244326+Jesus3jjj-20@users.noreply.github.com>
2022-01-25 17:54:14 +0100

    Add files via upload

---
+++ client.php
@@ -0,0 +1,26 @@
+<?php
+
+try {
+    $client = new SoapClient(null, array(
+            'uri' => 'http://practicesdwes.infinityfreeapp.com/Automoviles/service-automoviles.php',
+            'location' => 'http://practicesdwes.infinityfreeapp.com/Automoviles/service-automoviles.php',
+            'trace' => 1
+    )
+  );
+
+  $client->__setCookie('__test', 'd68af4541e8770807fac6af93f109119');
+
+  $auth_params = new stdClass();
+  $auth_params->username = "ies";
+  $auth_params->password = "daw";
+  $header_params = new SoapVar($auth_params,SOAP_ENC_OBJECT);
+  $header = new SoapHeader('http://practicesdwes.infinityfreeapp.com/Automoviles/service-automoviles.php', 'authenticate', $header_params, false);
+  $client->__setSoapHeaders(array($header));
+
+} catch (Exception $e) {
+    echo($client->__getLastResponse());
+    echo PHP_EOL;
+    echo($client->__getLastRequest());
+}
+
+?>
